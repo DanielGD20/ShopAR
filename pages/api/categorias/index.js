a93a1579f5aa32129f4e9ff9c67077232cbd99f8
Anthony Villegas <advillegas@uees.edu.ec>
2020-11-25 20:32:37 -0500

    pruebas con cors

--- pages/api/categorias/index.js
+++ pages/api/categorias/index.js
@@ -1,6 +1,22 @@
 import { firestore } from "../firebase/admin";
+import Cors from "cors";
+import initMiddleware from "../../lib/init-middleware";
+
+// Initialize the cors middleware
+const cors = initMiddleware(
+  // You can read more about the available options here: https://github.com/expressjs/cors#configuration-options
+  Cors({
+    // Only allow requests with GET, POST and OPTIONS
+    methods: ["GET", "POST", "OPTIONS"],
+  })
+);
 
 export default (req, res) => {
+  //cors
+  // Run cors
+  await cors(req, res);
+
+  //logica
   firestore
     .collection("categorias")
     .orderBy("orden", "asc")
