6fc85923a33d5775c21ca1039afdb6d12ef6caf1
Anthony Villegas <advillegas@uees.edu.ec>
2020-11-25 20:53:20 -0500

    pruebas con cors

--- next.config.js
+++ next.config.js
@@ -1,23 +1,20 @@
 // next.config.js
-const withPlugins = require("next-compose-plugins");
-const withVideos = require("next-videos");
 
-module.exports = withPlugins([[withVideos]], {
+module.exports = {
   experimental: { scss: true },
-  externals: ["aws-sdk", "commonjs2 firebase-admin"],
-  webpack: (config, { isServer }) => {
-    // Fixes npm packages that depend on `fs` module
-    if (!isServer) {
-      config.node = {
-        fs: "empty",
-        child_process: "empty",
-        net: "empty",
-        tls: "empty",
-      };
-    }
+  // webpack: (config, { isServer }) => {
+  //   // Fixes npm packages that depend on `fs` module
+  //   if (!isServer) {
+  //     config.node = {
+  //       fs: "empty",
+  //       child_process: "empty",
+  //       net: "empty",
+  //       tls: "empty",
+  //     };
+  //   }
 
-    return config;
-  },
+  //   return config;
+  // },
   async headers() {
     return [
       {
@@ -39,4 +36,4 @@ module.exports = withPlugins([[withVideos]], {
       },
     ];
   },
-});
+};
